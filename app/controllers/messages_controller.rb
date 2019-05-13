6f5ce2f77d2fe44530a530e8ea98eed6a9a15337
ryotaro-kohara <21ryotaro@gmail.com>
2019-05-13 20:38:38 +0900

    indicate.message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
 def index 
   @message = Message.new
-  @message = @group.messages.includes(:user)
+  @messages = @group.messages.includes(:user)
 end
 
 def show
@@ -13,7 +13,7 @@ end
 def create
   @message = @group.messages.new(message_params)
   if @message.save
-    redirect_to group_message_path(@group), notice: 'メッセージが送信されました'
+    redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
   else
     @messages = @group.messages.includes(:user)
     flash.now[:alert] = 'メッセージを入力してください。'
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,4 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
-  mount uploader :image, ImageUploader
 end
